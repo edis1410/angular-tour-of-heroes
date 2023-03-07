cf8c4de033f68f5922c9f65f40ec40238dd8bd88
Edis Fejzoski <edisfejzoski@gmail.com>
2023-03-07 14:41:15 +0100

    Modified heroes attributes and form

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
-  declarations: [HeroesComponent],
-  imports: [CommonModule,HeroesRoutingModule],
+  declarations: [HeroesComponent, HeroFormComponent],
+  imports: [CommonModule, HeroesRoutingModule, ReactiveFormsModule],
 })
 export class HeroesModule {}
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
+  { id: 12, name: 'Dr. Nice', age: 40, gender: 'male' },
+  { id: 13, name: 'Bombasto', age: 35, gender: 'male' },
+  { id: 14, name: 'Celeritas', age: 20, gender: 'male'  },
+  { id: 15, name: 'Magneta', age: 37, gender: 'female'  },
+  { id: 16, name: 'RubberMan', age: 31, gender: 'male'  },
+  { id: 17, name: 'Dynama', age: 23, gender: 'female'  },
+  { id: 18, name: 'Dr. IQ', age: 19, gender: 'male'  },
+  { id: 19, name: 'Magma', age: 23, gender: 'female'  },
+  { id: 20, name: 'Tornado', age: 35, gender: 'male'  },
 ];
