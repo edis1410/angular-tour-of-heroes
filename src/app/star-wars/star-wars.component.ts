9f8a03372fa4b13b8aefbec57f55df9bdebfa885
Edis Fejzoski <edisfejzoski@gmail.com>
2023-03-16 10:08:54 +0100

    Sorting by columns.

--- src/app/star-wars/star-wars.component.ts
+++ src/app/star-wars/star-wars.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, scan } from 'rxjs';
 import { StarWarsFacadeService } from '../star-wars-facade.service';
 import { Person } from '../person';
 
@@ -11,6 +11,48 @@ import { Person } from '../person';
 export class StarWarsComponent {
   public people$: Observable<Person[]> = this.swfs.getPeople$();
 
-  constructor(private swfs: StarWarsFacadeService) {}
-  
+  sortedColumn$ = new BehaviorSubject<string>('');
+
+  constructor(private swfs: StarWarsFacadeService) {
+    this.people$ = combineLatest([
+      this.swfs.getPeople$(),
+      this.sortDirection$,
+    ]).pipe(
+      map(([list, sort]) =>
+        !sort.col ? list : this.sortByColumn(list, sort.col, sort.dir)
+      )
+    );
+  }
+
+  sortDirection$ = this.sortedColumn$.pipe(
+    scan<string, { col: string; dir: string }>(
+      (sort, val) => {
+        return sort.col === val
+          ? { col: val, dir: sort.dir === 'desc' ? 'asc' : 'desc' }
+          : { col: val, dir: 'desc' };
+      },
+      { dir: 'desc', col: '' }
+    )
+  );
+
+  sortByColumn(
+    list: any[],
+    column: string,
+    direction = 'desc'
+  ): any[] {
+    let sortedArray = (list || []).sort((a, b) => {
+      if (a[column] > b[column]) {
+        return direction === 'desc' ? 1 : -1;
+      }
+      if (a[column] < b[column]) {
+        return direction === 'desc' ? -1 : 1;
+      }
+      return 0;
+    });
+    return sortedArray;
+  }
+
+  sortOn(column: string) {
+    this.sortedColumn$.next(column);
+  }
 }
