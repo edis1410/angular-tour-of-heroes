3d7a9a921f66951a40ea5b9b5216160eebf388d3
Edis Fejzoski <edisfejzoski@gmail.com>
2023-03-17 11:49:02 +0100

    Tailwind changes.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -7,7 +7,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css'],
+  styleUrls: ['../../../dist/output.css'],
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -4,7 +4,7 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css'],
+  styleUrls: ['../../../dist/output.css'],
 })
 export class MessagesComponent {
   constructor(public messageService: MessageService) {}
