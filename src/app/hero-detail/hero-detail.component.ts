bd598dfbaac1021f085d52c8f083a98504dc0876
Edis Fejzoski <edisfejzoski@gmail.com>
2023-03-21 13:02:10 +0100

    Made the detail form reactive.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,32 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService } from '../hero.service';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, switchMap, tap } from 'rxjs';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit{
   hero$: Observable<Hero> = this.route.params.pipe(
     switchMap((params: Params) => this.heroService.getHero(params['id']))
   );
+  
+    public heroDetailForm = this.fb.group({
+    id: this.fb.control<number | null>(null),
+    name: this.fb.control<string | null>(null, [Validators.required]),
+    age: this.fb.control<number | null>(null, [
+      Validators.min(0),
+      Validators.pattern('^[0-9]*$'),
+    ]),
+  });
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location
+    private location: Location,
+    private fb: FormBuilder
   ) {}
+  
+  ngOnInit(): void {
+    this.heroDetailForm.get('id')?.setValue(1);
+    this.heroDetailForm.get('name')?.setValue('neki');
+    this.heroDetailForm.get('age')?.setValue(20);
+  }
 
   goBack(): void {
     this.location.back();
   }
 
-  // save(): void {
-  //   if (this.hero) {
-  //     this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
-  //   }
-  // }
+  save(): void {
+    const heroFormData = this.heroDetailForm.getRawValue();
+      this.heroService.updateHero(heroFormData as Hero).subscribe(() => this.goBack());
+  }
 }
--- src/app/hero-detail/hero-detail.module.ts
+++ src/app/hero-detail/hero-detail.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroDetailRoutingModule } from './hero-detail-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeroDetailComponent],
-  imports: [CommonModule, FormsModule, HeroDetailRoutingModule],
+  imports: [CommonModule, FormsModule, HeroDetailRoutingModule, ReactiveFormsModule],
 })
 export class HeroDetailModule {}
